c824e18befbd977306d4b838ae8a87bb5b8d75cd
ruslan <rus.barik@gmail.com>
2018-06-10 12:25:35 +0300

    Delete task

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -76,8 +76,23 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
             // Called when a user swipes left or right on a ViewHolder
             @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
+                // TODO (1) Get the diskIO Executor from the instance of AppExecutors and
+                // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<TaskEntry> list = mAdapter.getTasks();
+                        mDb.taskDao().deleteTask(list.get(position));
+                        retrieveTasks();
+                    }
+                });
+
+                // TODO (3) get the position from the viewHolder parameter
+                // TODO (4) Call deleteTask in the taskDao with the task at that position
+                // TODO (6) Call retrieveTasks method to refresh the UI
             }
         }).attachToRecyclerView(mRecyclerView);
 
@@ -108,13 +123,17 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (5) Get the diskIO Executor from the instance of AppExecutors and
-        // call the diskIO execute method with a new Runnable and implement its run method
+        // TODO (5) Extract the logic to a retrieveTasks method so it can be reused
+        retrieveTasks();
+    }
+
+    private void retrieveTasks() {
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
                 final List<TaskEntry> tasks = mDb.taskDao().loadAllTasks();
-                // will be simplified later
+                // We will be able to simplify this once we learn more
+                // about Android Architecture Components
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -123,10 +142,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
                 });
             }
         });
-
-        // TODO (6) Move the logic into the run method and
-        // extract the list of tasks to a final variable
-        // TODO (7) Wrap the setTask call in a call to runOnUiThread
     }
 
     @Override
--- app/src/main/java/com/example/android/todolist/database/AppDatabase.java
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -22,7 +22,6 @@ public abstract class AppDatabase extends RoomDatabase {
                 Log.d(LOG_TAG, "Creating new database instance");
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         AppDatabase.class, AppDatabase.DATABASE_NAME)
-                        // TODO (1) Disable queries on main thread
                         .build();
             }
         }

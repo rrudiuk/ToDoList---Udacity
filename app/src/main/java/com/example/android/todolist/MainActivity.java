e88ac19feedbfad5cd817cfee90cf399c1dd9134
ruslan <rus.barik@gmail.com>
2018-06-10 12:32:48 +0300

    Add LiveData to AddTaskActivity

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -88,7 +88,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
                         int position = viewHolder.getAdapterPosition();
                         List<TaskEntry> tasks = mAdapter.getTasks();
                         mDb.taskDao().deleteTask(tasks.get(position));
-                        // TODO (6) Remove the call to retrieveTasks
                     }
                 });
             }
@@ -111,22 +110,16 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         });
 
         mDb = AppDatabase.getInstance(getApplicationContext());
-        // TODO (7) Call retrieveTasks from here and remove the onResume method
         retrieveTasks();
     }
 
     private void retrieveTasks() {
-        // TODO (4) Extract all this logic outside the Executor and remove the Executor
         Log.d(TAG, "Actively retrieving the tasks from the DataBase");
-        // TODO (3) Fix compile issue by wrapping the return type with LiveData
-        final LiveData<List<TaskEntry>> tasks = mDb.taskDao().loadAllTasks();
-        // TODO (5) Observe tasks and move the logic from runOnUiThread to onChanged
-        // We will be able to simplify this once we learn more
-        // about Android Architecture Components
+        LiveData<List<TaskEntry>> tasks = mDb.taskDao().loadAllTasks();
         tasks.observe(this, new Observer<List<TaskEntry>>() {
             @Override
             public void onChanged(@Nullable List<TaskEntry> taskEntries) {
-                Log.d(TAG, "Actively retrieving the tasks from the LiveData");
+                Log.d(TAG, "Receiving database update from LiveData");
                 mAdapter.setTasks(taskEntries);
             }
         });
